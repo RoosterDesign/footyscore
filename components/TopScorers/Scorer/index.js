8b0cb4a7c4148a967b6cd2d10f9a3797405c220a
roosterdesign <neil@roosterdesign.co.uk>
2019-09-23 08:02:21 +0100

    eslint

--- components/TopScorers/Scorer/index.js
+++ components/TopScorers/Scorer/index.js
@@ -1,14 +1,24 @@
+import PropTypes from 'prop-types';
+
 const Scorer = props => {
   return (
     <>
       <p>Scorer Name: {props.scorer.name}</p>
       <p>Goals Scored: {props.scorer.goals}</p>
 
-      <input type="number" value={props.scorer.goals} onChange={props.changed} />
+      <input
+        type="number"
+        value={props.scorer.goals}
+        onChange={props.changed}
+      />
 
       <hr />
     </>
   );
+};
+
+Scorer.propTypes = {
+  scorer: PropTypes.array.isRequired
 };
 
 export default Scorer;
